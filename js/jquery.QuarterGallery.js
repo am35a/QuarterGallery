c029a9edd813ac9f1b5a877da8d6a1886a48abc1
Arthur A. Selimov <am35a.piston@gmail.com>
2016-03-07 00:35:18 +0300

    #9: added action buttons are replaced

--- js/jquery.QuarterGallery.js
+++ js/jquery.QuarterGallery.js
@@ -1,5 +1,5 @@
 /*!
- * QuarterGallery 0.0.6
+ * QuarterGallery 0.0.7
  * https://github.com/am35a/QuarterGallery
  * MIT licensed
  *
@@ -18,7 +18,6 @@ WebFontConfig = { google: { families: [ 'Material+Icons' ] } };
     s.parentNode.insertBefore( wf, s );
 } )();
 
-
 /*-- the QuarterGallery JS code --*/
 var QG_galleryFolder = '',
     QG_tumbnailPrefix = '',
@@ -27,42 +26,47 @@ var QG_galleryFolder = '',
     QG_direction = 'none',
     QG_animationEnd = true,
     QG_openOnce = false,
-    QG_listTable = '';
-   
+    QG_listTable = '',
+    QG_zoom = false;
+
 $( function(){
     /*-- create QuarterGallery html code --*/
     $( '.qg-container' ).html(
         '<div class="qg-container__controls">' +
         '    <i class="material-icons qg-first-image qg-first-image--action">first_page</i>' +
         '    <i class="material-icons qg-last-image qg-last-image--action">last_page</i>' +
+        '    <i class="material-icons qg-zoom qg-zoom--action">zoom_in</i>' +
         '    <i class="material-icons qg-fullscreen qg-fullscreen--action">fullscreen</i>' +
         '    <i class="material-icons qg-container-close qg-container-close--action">close</i>' +
         '</div>' +
-        '<div class="qg-container__quarter-1 qg-scroll-left--action">' +
-        '    <div class="qg-quarter__slide-1">' +
-        '        <img class="qg-image-shrink" src="" onerror="qgImgReload( true )">' +
-        '    </div>' +
-        '</div>' +
-        '<div class="qg-container__quarter-2 qg-scroll-right--action">' +
-        '    <div class="qg-quarter__slide-2">' +
-        '        <img class="qg-image-shrink" src="">' +
-        '    </div>' +
-        '</div>' +
-        '<div class="qg-container__quarter-3 qg-scroll-left--action">' +
-        '    <div class="qg-quarter__slide-3">' +
-        '        <img class="qg-image-shrink" src="">' +
-        '    </div>' +
-        '</div>' +
-        '<div class="qg-container__quarter-4 qg-scroll-right--action">' +
-        '    <div class="qg-quarter__slide-4">' +
-        '        <img class="qg-image-shrink" src="" onload="qgOnLoadImage()">' +
-        '    </div>' +
+        '<div>' +
+        '   <div class="qg-container__quarter-1 qg-scroll-left--action">' +
+        '       <div class="qg-quarter__slide-1">' +
+        '           <img class="qg-image-shrink" src="" onerror="qgImgReload( true )">' +
+        '       </div>' +
+        '   </div>' +
+        '   <div class="qg-container__quarter-2 qg-scroll-right--action">' +
+        '       <div class="qg-quarter__slide-2">' +
+        '           <img class="qg-image-shrink" src="">' +
+        '       </div>' +
+        '   </div>' +
+        '   <div class="qg-container__quarter-3 qg-scroll-left--action">' +
+        '       <div class="qg-quarter__slide-3">' +
+        '           <img class="qg-image-shrink" src="">' +
+        '       </div>' +
+        '   </div>' +
+        '   <div class="qg-container__quarter-4 qg-scroll-right--action">' +
+        '       <div class="qg-quarter__slide-4">' +
+        '           <img class="qg-image-shrink" src="" onload="qgOnLoadImage()">' +
+        '       </div>' +
+        '   </div>' +
         '</div>' +
         '<i class="material-icons md-48 qg-scroll-left qg-scroll-left--action">keyboard_arrow_left</i>' +
         '<i class="material-icons md-48 qg-scroll-right qg-scroll-right--action">keyboard_arrow_right</i>' +
-        '<div class="qg_containet__list-table"></div>'
+        '<div class="qg_containet__list-table"></div>' +
+        '<div class="qg_containet__gallery-info"></div>'
     );
-    
+
     /*-- jump to first or last images --*/
     $( '.qg-first-image--action' ).on( 'click', function(){
         if ( QG_animationEnd && QG_imgNum > 1 ){
@@ -83,26 +87,47 @@ $( function(){
         }
     });
 
-    /*-- fullscreen enter or exit --*/
-    var fullscreen = false;
-    $( '.qg-fullscreen--action' ).on( 'click', function(){
-        if ( !fullscreen ){
-            $( '.qg-fullscreen' ).html( 'fullscreen_exit' );
+    /*-- zoom enter or exit --*/
+    $( '.qg-zoom--action' ).on( 'click', function(){
+        if ( !QG_zoom ){
+            $( '.qg-zoom' ).html( 'zoom_out' );
             $( '[class*=qg-quarter__slide-] img' )
                 .removeClass( 'qg-image-shrink' )
                 .addClass( 'qg-image-expand' );
         }
         else {
-            $( '.qg-fullscreen' ).html( 'fullscreen' );
+            $( '.qg-zoom' ).html( 'zoom_in' );
             $( '[class*=qg-quarter__slide-] img' )
                 .removeClass( 'qg-image-expand' )
                 .addClass( 'qg-image-shrink' );
         }
-        fullscreen = !fullscreen;
+        QG_zoom = !QG_zoom;
     });
+    
+    /*-- fullscreen enter or exit --*/
+    $( '.qg-fullscreen--action' ).on( 'click', function(){
+        qgFullScreen();
+    });
+    function qgFullScreen() {
+        if ( !document.fullscreenElement &&    // alternative standard method
+             !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
+            if ( document.documentElement.requestFullscreen ) document.documentElement.requestFullscreen();
+            else if ( document.documentElement.msRequestFullscreen ) document.documentElement.msRequestFullscreen();
+            else if ( document.documentElement.mozRequestFullScreen ) document.documentElement.mozRequestFullScreen();
+            else if ( document.documentElement.webkitRequestFullscreen ) document.documentElement.webkitRequestFullscreen( Element.ALLOW_KEYBOARD_INPUT );
+            $( '.qg-fullscreen' ).html( 'fullscreen_exit' );
+        } else {
+            if ( document.exitFullscreen ) document.exitFullscreen();
+            else if ( document.msExitFullscreen ) document.msExitFullscreen();
+            else if ( document.mozCancelFullScreen ) document.mozCancelFullScreen();
+            else if ( document.webkitExitFullscreen ) document.webkitExitFullscreen();
+            $( '.qg-fullscreen' ).html( 'fullscreen' );
+        }
+    }
     
     /*-- close QuarterGallery view --*/
     $( '.qg-container-close--action' ).on( 'click', function(){
+        if ( $( '.qg-fullscreen' ).html() == 'fullscreen_exit' ) qgFullScreen();
         $( '.qg-quarter__slide-1, .qg-quarter__slide-3' ).css( 'transform', 'translateX( -50vw )' );
         $( '.qg-quarter__slide-2, .qg-quarter__slide-4' ).css( 'transform', 'translateX( 50vw )' );
         setTimeout( function(){
@@ -131,9 +156,10 @@ $( function(){
     /*-- gallery open by image click --*/
     $( 'img.QuarterGallery' ).on( 'click', function(){
             var str = $( this ).attr( 'src' ),
-                cutFirst = str.lastIndexOf( QG_tumbnailPrefix ),
-                cutLast = cutFirst + QG_tumbnailPrefix.length;
+                cutFirst = str.lastIndexOf( QG_tumbnailPrefix );
+                //cutLast = cutFirst + QG_tumbnailPrefix.length;
             QG_imgNum = +str.slice( cutFirst + QG_tumbnailPrefix.length, -4 );
+            QG_galleryFolder = str.slice( 0, cutFirst);
             if( $('.QuarterGallery').length >= QG_imgLast )
                 QG_imgLast = $('.QuarterGallery').length;
             QG_direction = 'none';
@@ -225,7 +251,16 @@ function qgRandomInteger( min, max ){
 }
 
 function QuarterGallery(){
-    QG_openOnce = true;
+    if( !QG_openOnce ){
+        $.get( QG_galleryFolder + 'info.txt', function( data ){
+            $( '.qg_containet__gallery-info' ).show().html( data );
+            //alert( data );
+        }, 'text' )
+        .fail(function() {
+            $( '.qg_containet__gallery-info' ).hide();
+        });
+        QG_openOnce = true;
+    }
     QG_direction = 'none';
     $( '.qg-container' ).fadeIn( 250, function() {
         qgImgReload( false );
